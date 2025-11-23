4ac4a1bf4e831ad785e7ffe8ff7bcf8330c4e175
johnvictorfs <37747572+johnvictorfs@users.noreply.github.com>
2025-11-23 20:00:34 -0300

    add db auth token

--- src/env.js
+++ src/env.js
@@ -14,6 +14,7 @@ export const env = createEnv({
 		BETTER_AUTH_GITHUB_CLIENT_ID: z.string(),
 		BETTER_AUTH_GITHUB_CLIENT_SECRET: z.string(),
 		DATABASE_URL: z.string().url(),
+		DATABASE_AUTH_TOKEN: z.string().optional(),
 		NODE_ENV: z
 			.enum(["development", "test", "production"])
 			.default("development"),
@@ -38,6 +39,7 @@ export const env = createEnv({
 		BETTER_AUTH_GITHUB_CLIENT_SECRET:
 			process.env.BETTER_AUTH_GITHUB_CLIENT_SECRET,
 		DATABASE_URL: process.env.DATABASE_URL,
+		DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
 		NODE_ENV: process.env.NODE_ENV,
 	},
 	/**
